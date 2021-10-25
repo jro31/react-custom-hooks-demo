42cd7be1f461e77b665c4bb14833fd32bb994000
Jethro Williams <jethrowilliams@outlook.com>
2021-10-25 09:21:43 +0100

    Allow the custom hook to accept a parameter (with default value) to also use it in the BackwardCounter

--- src/hooks/use-counter.js
+++ src/hooks/use-counter.js
@@ -1,16 +1,23 @@
 import { useState, useEffect } from 'react';
 
 // The function name MUST start with 'use' when creating a custom hook
-const useCounter = () => {
+const useCounter = (forwards = true) => { // Accepting the param 'forward' with a default value of true
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
+      if (forwards) {
+        setCounter((prevCounter) => prevCounter + 1);
+      } else {
+        setCounter((prevCounter) => prevCounter - 1);
+      };
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [forwards]);
+  // 'forwards' is a dependency because
+  // it's not defined inside the effect function and
+  // it's not defined outside our custom hook (we receive it as a parameter)
 
   return counter;
 };
